be867c39b7528095ba9cad03f3f94e4b89a9410a
unknown <rezzamuhammad12@gmail.com>
2020-04-18 14:09:27 +0700

    Hasil ChatApp

---
+++ app/src/main/java/unpas/ac/id/MainActivity.java
@@ -0,0 +1,95 @@
+package unpas.ac.id;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebSocketClient mWebSocketClient;
+    private EditText mEditTextChats;
+    private EditText mEditTextMessage;
+    private Button mButtonSend;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mEditTextChats = findViewById(R.id.editTextChats);
+        mEditTextMessage = findViewById(R.id.editTextMessage);
+        mButtonSend = findViewById(R.id.buttonSend);
+
+        connectWebSocket();
+
+        mButtonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attemptSend();
+            }
+        });
+    }
+
+    private void connectWebSocket() {
+        URI uri;
+        try {
+            uri = new URI("ws://192.168.43.197:8887");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        mWebSocketClient = new WebSocketClient(uri) {
+            @Override
+            public void onOpen(ServerHandshake serverHandshake) {
+                Log.i("Websocket", "Opened");
+                mWebSocketClient.send("Hello from " + Build.MANUFACTURER + " " + Build.MODEL);
+            }
+
+            @Override
+            public void onMessage(String s) {
+                final String message = s;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mEditTextChats.append(message + "\n");
+                    }
+                });
+            }
+
+            @Override
+            public void onClose(int i, String s, boolean b) {
+                Log.i("Websocket", "Closed " + s);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.i("Websocket", "Error" + e.getMessage());
+            }
+        };
+        mWebSocketClient.connect();
+    }
+
+    private void attemptSend() {
+        String message = mEditTextMessage.getText().toString().trim();
+        if (TextUtils.isEmpty(message)) {
+            return;
+        }
+
+        mEditTextMessage.setText("");
+        mWebSocketClient.send(message);
+    }
+}
